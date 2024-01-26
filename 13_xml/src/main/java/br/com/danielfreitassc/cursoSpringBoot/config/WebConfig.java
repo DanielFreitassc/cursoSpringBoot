ea99a268e1ae2e55c2ad585b4c94e98e00aa1464
danielfreitassc <danielfreitassc@gmail.com>
2024-01-25 23:06:16 -0300

    nao funcionou

--- 13_xml/src/main/java/br/com/danielfreitassc/cursoSpringBoot/config/WebConfig.java
+++ 13_xml/src/main/java/br/com/danielfreitassc/cursoSpringBoot/config/WebConfig.java
@@ -1,22 +1,37 @@
 package br.com.danielfreitassc.cursoSpringBoot.config;
 
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        //http://localhost:8080/api/person/v1?mediaType=xml
+         /* Via Query PARAM http://localhost:8080/api/person/v1?mediaType=xml
         configurer.favorParameter(true)
-        .parameterName("mediaType").ignoreAcceptHeader(true)
-        .useRegisteredExtensionsOnly(false)
-        .defaultContentType(MediaType.APPLICATION_JSON)
-        .mediaType("json", MediaType.APPLICATION_JSON)
-        .mediaType("xml", MediaType.APPLICATION_XML);
+			.parameterName("mediaType").ignoreAcceptHeader(true)
+			.useRegisteredExtensionsOnly(false)
+			.defaultContentType(MediaType.APPLICATION_JSON)
+			.mediaType("json", MediaType.APPLICATION_JSON)
+			.mediaType("xml", MediaType.APPLICATION_XML);
+        
+
+        // VIA HEADER PARAM. http://localhost:8080/api/person/v1?mediaType=xml
+        
+        configurer.favorParameter(false)
+			.ignoreAcceptHeader(false)
+			.useRegisteredExtensionsOnly(false)
+			.defaultContentType(MediaType.APPLICATION_JSON)
+			.mediaType("json", MediaType.APPLICATION_JSON)
+			.mediaType("xml", MediaType.APPLICATION_XML);
+        */
     }
-    
+       
 }
